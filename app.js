d6f7146fca8123a5ccce99ea3374d979d06db112
Bayu Krisna <bayukrisna231@gmail.com>
2026-09-14 14:31:56 +0700

    feat: add documentation api with swagger

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const express = require('express');
 const app = express();
 const compression = require("compression");
 const PORT = 3000;
+const { swaggerUi, swaggerDocument } = require("./swagger/swagger");
 
 const userRoutes = require("./routes/user.route");
 const authRoutes = require("./routes/auth.route");
 
+app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerDocument)
+);
 app.use(compression());
 app.use(express.json());
 
